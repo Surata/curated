7cd3624017f845202c095a3d6eec252454b16772
Surata <e.surata@gmail.com>
2016-08-04 18:28:36 +0700

    Add password validation

--- application/models/Account.php
+++ application/models/Account.php
@@ -21,4 +21,19 @@ class Account extends MY_Model {
         $this->db->insert('account', $this);
     }
 
+    public function search_by_username($username)
+    {
+        $username = $this->params()['username'];
+        $account = $this->db->get_where('account', array('username' => $username))->row()
+
+        return $account
+    }
+
+    public function authorize_account($account)
+    {
+        $password = $this->params()['password']
+        $is_valid = password_verify($password, $account->password_hash)
+        return $is_valid
+    }
+
 }
